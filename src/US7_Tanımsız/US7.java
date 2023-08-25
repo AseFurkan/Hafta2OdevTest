055f20f4108842feadc480265d0ec69711e8b26d
1987adem <ademozden0454@gmail.com>
2023-08-25 21:53:59 +0300

    tatile gittim

--- src/US7_Tanımsız/US7.java
+++ src/US7_Tanımsız/US7.java
@@ -3,5 +3,6 @@ package US7_Tanımsız;
 public class US7 {
     public static void main(String[] args) {
         System.out.println("ben adem");
+        System.out.println("tatile gittim");
     }
 }
