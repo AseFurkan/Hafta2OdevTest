8ff88ed483b61366256fad7a67d62c23ce1eb1ed
alp2344 <aygunalpr@gmail.com>
2023-08-27 23:11:58 +0300

    BaseDriver.java

--- src/US1_Alper/Us1.java
+++ src/US1_Alper/Us1.java
@@ -1,6 +1,19 @@
 package US1_Alper;
 
-public class Us1 {
+import org.junit.Test;
+
+public class Us1 extends Utlity.BaseDriver {
 
 
-}
+        @Test
+    public void Test1(){
+
+driver.get("https://demowebshop.tricentis.com/");
+
+        }
+
+
+    }
+
+
+
